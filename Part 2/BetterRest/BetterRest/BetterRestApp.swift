84c91efbc7fd9f18607fb15c8cd69f66629dd220
Kev Lee <kevinleect002@gmail.com>
2022-08-27 13:23:57 +0800

    Project 4

---
+++ Part 2/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kev Lee on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
